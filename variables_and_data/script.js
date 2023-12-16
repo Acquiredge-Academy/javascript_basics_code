b23c94ca0984f166188823502de804ee5df41027
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2023-12-16 03:57:29 +0000

    Constants

--- variables_and_data/script.js
+++ variables_and_data/script.js
@@ -2,5 +2,6 @@ let myName = 'Allan'; // String
 let age = 24; // Number
 let isCurrentlyHungry = true; // Boolean
 let middleName = null; // Null
+const eyeColor = "brown"; // Constant
 
-console.log(age);
+console.log(eyeColor);
