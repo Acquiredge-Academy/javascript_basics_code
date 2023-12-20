1f0077b1912ca78193c714486921bc853a463369
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2023-12-20 02:55:59 +0000

    Operators

---
+++ operators/script.js
@@ -0,0 +1,24 @@
+// Arithmetic
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Increment
+// console.log(x++);
+// console.log(x);
+
+// Decrement
+// console.log(--y);
+// console.log(y);
+
+// Assignment
+// x = x + 5;
+x += 5;
+
+console.log(x);
