0cac0ab33f42883e00edf5c612586b685bac4eda
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2023-12-20 01:59:28 +0000

    Arrays Basics

--- variables_and_data/script.js
+++ variables_and_data/script.js
@@ -17,4 +17,11 @@ let propName = "hungry";
 person.eyeColor = "black";
 person[propName] = false;
 
-console.log(person);
+let fruits = ["apples", "bananas", "oranges", "grapes"];
+fruits[2] = "tangerines";
+fruits.push("oranges");
+let popped = fruits.pop();
+
+console.log(fruits);
+console.log(popped);
+console.log(fruits.length);
