6f1a5e95fc0f2913aec5d15540da717d136df01b
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2023-12-22 04:54:51 +0000

    Calculating a value and PREP

--- functions_and_logic/functions.js
+++ functions_and_logic/functions.js
@@ -2,5 +2,24 @@ function greet(firstName, lastName) {
     console.log("Hello " + firstName + " " + lastName);
 }
 
-greet("John", "Smith");
-greet("Mark", "Smith");
+// greet("John", "Smith");
+// greet("Mark", "Smith");
+
+// Declare a function that calculates the area of a rectangle
+// Given the length and height
+// PREP: Parameters, Return, Examples, Pseudocode
+
+function areaOfRectangle(length, height) {
+    if(length < 0 || height < 0){
+        return -1;
+    }
+    let area;
+    // Set area equal to length times height
+    area = length * height;
+    return area;
+}
+
+console.log(areaOfRectangle(4, 5)); // 20
+console.log(areaOfRectangle(3, 4)); // 12
+console.log(areaOfRectangle(10, 10)); // 100
+console.log(areaOfRectangle(-3, 6)); // -1
