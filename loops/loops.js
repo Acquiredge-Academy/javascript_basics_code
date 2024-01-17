16f302508a2f54c5fc041acbbfa514e904ba52a2
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-01-17 01:23:23 +0000

    While loops

--- loops/loops.js
+++ loops/loops.js
@@ -1,4 +1,10 @@
 // Display "Hello World" 5 times in the console
-for(let i = 1; i <= 5; i++) {
-    console.log("Hello World " + i);
+// for(let i = 1; i <= 5; i++) {
+//     console.log("Hello World");
+// }
+
+let i = 1;
+while(i <= 5) {
+    console.log("Hello World");
+    i++;
 }
