e485b5a4d2a51fb383fb0becd4a7528a197f7f18
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-01-22 00:41:39 +0000

    Adding exercise2.js and prompt

---
+++ examples_and_exercises/exercise2.js
@@ -0,0 +1,4 @@
+// Even and Odd Numbers
+// Write a function that takes a number as the limit.
+// The function should print out all numbers from 0 to that limit, inclusive.
+// For each number, it should also print whether it is even or odd.
