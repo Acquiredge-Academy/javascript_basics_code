f50eef42b482458207bd8c1bffa264939c9afaca
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-01-26 04:07:48 +0000

    Adding initial code for exercise 4 sum odds

---
+++ examples_and_exercises/exercise4.js
@@ -0,0 +1,15 @@
+// Exercise: Sum Limit
+// Write a function sumOdd that takes a limit
+// and returns the sum of all odd integers up to the limit, inclusive.
+// The limit can not be negative.
+
+function sumLimit(limit) {
+    let sum = 0;
+
+    return sum;
+}
+
+console.log(sumLimit(0)); // 0
+console.log(sumLimit(3)); // 4
+console.log(sumLimit(8)); // 16
+console.log(sumLimit(19)); // 100
