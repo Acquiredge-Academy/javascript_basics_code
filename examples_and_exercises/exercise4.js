86326985b82ea08d8991ff0d1206161c9c91f2b8
AcquirEdge <130426290+AcquirEdge@users.noreply.github.com>
2024-01-27 02:35:48 +0000

    Fixing typo from sumLimit to sumOdd

--- examples_and_exercises/exercise4.js
+++ examples_and_exercises/exercise4.js
@@ -1,17 +1,16 @@
-// Exercise: Sum Limit
+// Exercise: Sum Odd
 // Write a function sumOdd that takes a limit
 // and returns the sum of all odd integers up to the limit, inclusive.
 // The limit can not be negative.
 
-function sumLimit(limit) {
+function sumOdd(limit) {
     let sum = 0;
-    for(let i = 1; i <= limit; i+=2){
+    for(let i = 1; i <= limit; i+=2)
         sum += i;
-    }
     return sum;
 }
 
-console.log(sumLimit(0)); // 0
-console.log(sumLimit(3)); // 4
-console.log(sumLimit(8)); // 16
-console.log(sumLimit(19)); // 100
+console.log(sumOdd(0)); // 0
+console.log(sumOdd(3)); // 4
+console.log(sumOdd(8)); // 16
+console.log(sumOdd(19)); // 100
